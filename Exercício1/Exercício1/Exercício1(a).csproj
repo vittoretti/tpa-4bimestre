ef89822a24fbd5531f950fb8d6358dc1c1530a2f
Vittoretti <matheus.costa335@etec.sp.gov.br>
2025-12-04 15:02:14 -0300

    Add files via upload

---
+++ Exercício1/Exercício1/Exercício1(a).csproj
@@ -0,0 +1,34 @@
+﻿class Program
+{
+
+    public struct data
+    {
+        public int dia;
+        public int mes;
+        public int ano;
+    }
+
+    public static void Main(string[] args)
+    {
+        data dataatual;
+        Console.WriteLine("Informe a data atual:");
+        Console.Write("Informe o dia:");
+        dataatual.dia = int.Parse(Console.ReadLine());
+        Console.Write("Informe o mês:");
+        dataatual.mes = int.Parse(Console.ReadLine());
+        Console.Write("Informe o ano:");
+        dataatual.ano = int.Parse(Console.ReadLine());
+        if (dataatual.dia <= 31 && dataatual.mes <= 12 && dataatual.ano <= 2025)
+        {
+            Console.WriteLine("A data informada é válida.");
+        }
+        else if (dataatual.dia <= 28 && dataatual.mes == 2 && dataatual.ano <= 2025) 
+        {
+            Console.WriteLine("A data informada é válida.");
+        }
+        else 
+        {
+            Console.WriteLine("A data informada é inválida.");
+        }
+    }
+}
---
+++ Exercício2/Exercício2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
+using System.Xml.Schema;
+
+class Program 
+{
+  public struct aluno
+    {
+        public string nome;
+        public double nota1;
+        public double nota2;
+        public double media;
+        public bool aprovacao;
+    }
+  static void Main(string[] args) 
+    {
+        const int TAM = 15;
+        aluno[]alunos = new aluno[TAM];
+        int contador;
+
+        for (contador = 0; contador< TAM; contador ++) 
+        {
+            Console.Write("Digite o nome do {0}° aluno:", contador + 1);
+            alunos[contador].nome = (Console.ReadLine());
+            Console.Write("Digite a primeira nota:");
+            alunos[contador].nota1 = double.Parse(Console.ReadLine());
+            Console.Write("Digite a segunda nota:");
+            alunos[contador].nota2 = double.Parse(Console.ReadLine());
+            alunos[contador].media = alunos[contador].nota1 + alunos[contador].nota2 / 2;
+            if (alunos[contador].media >= 7)
+            {
+                alunos[contador].aprovacao = true;
+            }
+            else 
+            {
+                alunos[contador].aprovacao = false;
+            }
+        }
+        Console.Clear();
+        Console.WriteLine("Boletim dos Alunos:");
+        for (contador = 0; contador < TAM; contador++)
+        {
+            if (alunos[contador].aprovacao == true)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("\nNome do {0}º aluno: {1}", contador + 1, alunos[contador].nome);
+                Console.WriteLine("Nota 1: {0:N1}", alunos[contador].nota1);
+                Console.WriteLine("Nota 2: {0:N1}" , alunos[contador].nota2);
+                Console.WriteLine("Média: {0:N1}", alunos[contador].media);
+                Console.WriteLine("Situação: Aprovado");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nNome do {0} aluno: {1}",contador + 1, alunos[contador].nome);
+                Console.WriteLine("Nota 1: {0:N1}", alunos[contador].nota1);
+                Console.WriteLine("Nota 2: {0:N1}", alunos[contador].nota2);
+                Console.WriteLine("Média: {0:N1}", alunos[contador].media);
+                Console.WriteLine("Situação: Reprovado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
